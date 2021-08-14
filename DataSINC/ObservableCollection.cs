aee10f7742642caef3c69971162a20c6a1a3835a
daredloco <deadlybeatz@hotmail.com>
2021-08-14 02:42:58 -0300

    Added ObservableList

---
+++ DataSINC/ObservableCollection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSINC
+{
+	public class ObservableList<T> : ObservableCollection<T>
+	{
+		public ObservableList()
+		{
+
+		}
+
+		public ObservableList(IEnumerable<T> collection) : base(collection)
+		{
+
+		}
+
+		public ObservableList(List<T> list) : base(list)
+		{
+
+		}
+
+		public int FindIndex(Predicate<T> obj)
+		{
+			int index = 0;
+		
+			foreach(T item in base.Items)
+			{
+				if(item.Equals((T)obj.Clone()))
+				{
+					return index;
+				}
+				index++;
+			}
+			return -1;
+		}
+	}
+}
