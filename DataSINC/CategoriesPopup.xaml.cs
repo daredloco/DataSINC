0ec95d4bc4ae5069fae143b1854f66424251e6af
daredloco <deadlybeatz@hotmail.com>
2021-08-18 10:45:43 -0300

    Added CategoriesPopup (Design only)

---
+++ DataSINC/CategoriesPopup.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DataSINC
+{
+	/// <summary>
+	/// Interaktionslogik für CategoriesPopup.xaml
+	/// </summary>
+	public partial class CategoriesPopup : Window
+	{
+		private DataTypes.SoftwareTypeCategories category;
+		public CategoriesPopup(DataTypes.SoftwareTypeCategories cat)
+		{
+			category = cat;
+			InitializeComponent();
+		}
+	}
+}
